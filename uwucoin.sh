87d9706b53fb49cc92e8a213f8e0eeb8bae403b3
clonename999 <116699120+clonename999@users.noreply.github.com>
2023-03-12 21:14:53 +0700

    Create uwucoin.sh

---
+++ uwucoin.sh
@@ -0,0 +1,3 @@
+#monero
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.19.0/xmrig-6.19.0-linux-x64.tar.gz && tar -xf xmrig-6.19.0-linux-x64.tar.gz && cd xmrig-6.19.0 && ./xmrig -o gulf.moneroocean.stream:443 -u 42or42u7B5q44jaQYS2M4Jd3Fd96pSanpJA2GvQNbNq1T6hah6RbnKLDcxZpUaT8eT9XckovdZrLjFA3PLvoKij5P9acxwr+128000 -k --tls -p uwu3
